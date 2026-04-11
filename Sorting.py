7e52c5b1ab152b66486ef8c8e8d9a8fdd3645459
Avinash Gupta <avinashguptaa0.3@gmail.com>
2026-04-11 12:27:07 +0545

    selection sort and bubble sort

---
+++ Sorting.py
@@ -0,0 +1,35 @@
+#Sorting
+
+#1. Selection Sort
+
+def Sorting(array):
+    num=len(array)
+    for i in range(num):
+        minIndex=i
+        for j in range(i+1,num):
+            if array[minIndex]>array[j]:
+                minIndex=j
+        array[i],array[minIndex]=array[minIndex],array[i]
+    print(array)
+
+array=[1,7,9,2,5,3,4,8,6]
+
+Sorting(array)
+
+
+#2. Bubble Sort
+
+def BubbleSorting(array):
+    num=len(array)
+    isSwapped=False
+    for i in range(num):
+        for j in range(0,num-i-1):
+            if array[j]>array[j+1]:
+                array[j],array[j+1]=array[j+1],array[j]
+                isSwapped=True
+        if not isSwapped:
+            break
+    print(array)
+
+array=[1,7,9,2,5,3,4]
+BubbleSorting(array)
