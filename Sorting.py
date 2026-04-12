aa2f4e4bfdae3150fe65d6f7ff0a0ae702af5c27
Avinash Gupta <avinashguptaa0.3@gmail.com>
2026-04-12 08:50:06 +0545

    improvement in game

--- Sorting.py
+++ Sorting.py
@@ -1,52 +1,52 @@
-# #Sorting
+#Sorting
 
-# #1. Selection Sort
+#1. Selection Sort
 
-# def Sorting(array):
-#     num=len(array)
-#     for i in range(num):
-#         minIndex=i
-#         for j in range(i+1,num):
-#             if array[minIndex]>array[j]:
-#                 minIndex=j
-#         array[i],array[minIndex]=array[minIndex],array[i]
-#     print(array)
+def Sorting(array):
+    num=len(array)
+    for i in range(num):
+        minIndex=i
+        for j in range(i+1,num):
+            if array[minIndex]>array[j]:
+                minIndex=j
+        array[i],array[minIndex]=array[minIndex],array[i]
+    print(array)
 
-# array=[1,7,9,2,5,3,4,8,6]
+array=[1,7,9,2,5,3,4,8,6]
 
-# Sorting(array)
-
-
-# #2. Bubble Sort
-
-# def BubbleSorting(array):
-#     num=len(array)
-#     isSwapped=False
-#     for i in range(num):
-#         for j in range(0,num-i-1):
-#             if array[j]>array[j+1]:
-#                 array[j],array[j+1]=array[j+1],array[j]
-#                 isSwapped=True
-#         if not isSwapped:
-#             break
-#     print(array)
-
-# array=[1,7,9,2,5,3,4]
-# BubbleSorting(array)
+Sorting(array)
 
 
-# def insertionSort(array):
-#     num=len(array)
-#     for i in range(1,num):
-#         key=array[i]
-#         j=i-1
-#         while j>=0 and key<array[j]:
-#             array[j+1]=array[j]
-#             j-=1
-#         array[j+1]=key
-#     print(array)
-# array=[11,5,7,1,4,9]
-# insertionSort(array)
+#2. Bubble Sort
+
+def BubbleSorting(array):
+    num=len(array)
+    isSwapped=False
+    for i in range(num):
+        for j in range(0,num-i-1):
+            if array[j]>array[j+1]:
+                array[j],array[j+1]=array[j+1],array[j]
+                isSwapped=True
+        if not isSwapped:
+            break
+    print(array)
+
+array=[1,7,9,2,5,3,4]
+BubbleSorting(array)
+
+
+def insertionSort(array):
+    num=len(array)
+    for i in range(1,num):
+        key=array[i]
+        j=i-1
+        while j>=0 and key<array[j]:
+            array[j+1]=array[j]
+            j-=1
+        array[j+1]=key
+    print(array)
+array=[11,5,7,1,4,9]
+insertionSort(array)
 
 
 
@@ -87,3 +87,6 @@ def mergeSort(array):
 array=[3,7,9,2,4,7,9]
 returnAnswer=mergeSort(array)
 print(returnAnswer)
+
+
+#5. Quick sort
--- test.py
+++ test.py
@@ -1,34 +1,46 @@
 import random
-def GameStart(num):
- points=0
- while True:
-      operator=['+',"-","*","//","%"]
-      number1=random.randint(1,num)
-      number2=random.randint(1,num)
+operations={
+         '+': lambda a,b :a+b,
+         '-':lambda a,b:a-b,
+         '*':lambda a,b:a*b,
+         '//':lambda a,b:a//b,
+         '%':lambda a,b:a%b
+      }
+
+def calculateResult(op,num1,num2):
+  return operations[op](num1,num2)
+
+
+def generateQuestion():
+   name=input("Enter Your Name :").upper().strip() 
+   numberRange=int(input("Enter the Number Range : "))
+   operator=['+',"-","*","//","%"]
+   points,level=0,0
+   while True:
       op=random.choice(operator)
-      userAns=int(input(f"{number1} {op} {number2} = "))
-      result=0
-      match(op):
-         case '+':
-            result=number1+number2
-         case '-':
-            result=number1-number2
-         case '*':
-            result=number1*number2
-         case '//':
-            result=number1//number2
-         case '%':
-            result=number1%number2
-         case _:
-            print("Something Went Wrong ")
+      num1=random.randint(1,numberRange)
+      num2=random.randint(1,numberRange)
+      try:
+         userAns=int(input(f"{num1} {op} {num2} = "))
+      except:
+         print("Enter Valid Value ")
+         continue
+      result=calculateResult(op,num1,num2)
       if userAns==result:
          points+=1
+         level+=1
       else:
-        print(f"You Miss The Game !!\n with Points : {points}")
+        print(f"Mr/Ms. {name}")
+        print(f"You Miss The Game !!\nwith Points : {points}")
         print(f"The Correct Answer Is : {result}")
         break
-        
-         
+      if level%5==0:
+         print(f"Mr/Ms. {name}")
+         print(f"Wow Youe Pass the Level {level//5}")
+         print(f"The Total Points you Get : {points}")
+         user=input("Want to Continue The Game : (yes/no) ? ").lower()
+         if user=="no":
+            break 
 
-number=int(input("Enter Any Number : "))
-GameStart(number)
+      
+generateQuestion()
